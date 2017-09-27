47e7acfde44501ad8e93c1a72e782afc50b3b199
Bimochan Shrestha <bmochan@gmail.com>
2017-09-27 11:36:13 +0545

    Dynamic days and comments add

--- fiscal-year.php
+++ fiscal-year.php
@@ -1,14 +1,57 @@
 <?php
-$expExploding = explode("-", $formatted_date_Op_date);
-if ($expExploding[1] > 7) {
-    $newExpYear = $expExploding[0] + 1 + $paid_years;
-} elseif ($expExploding[1] >= 7 && $expExploding[2] < 16) {
-    $newExpYear = $expExploding[0] + $paid_years;
-} elseif ($expExploding[1] >= 7 && $expExploding[2] > 16) {
-    $newExpYear = $expExploding[0] + 1 + $paid_years;
-} elseif ($expExploding[1] >= 7 && $expExploding[2] >= 16) {
-    $newExpYear = $expExploding[0] + $paid_years;
-} else {
-    $newExpYear = $expExploding[0] + $paid_years;
-}
-$_expiryDate = $newExpYear . '-' . '07' . '-' . '16'; 
+/* corresponding fiscal days of AD years. Month will be 07-> July*/
+$fiscalYear=array(
+            "2011"=>16,
+            "2012"=>15,
+            "2013"=>15,
+            "2014"=>16,
+            "2015"=>16,
+            "2016"=>15,
+            "2017"=>15,
+            "2018"=>16,
+            "2019"=>16,
+            "2020"=>15,
+            "2021"=>15,
+            "2022"=>16,
+            "2023"=>16,
+            "2024"=>15,
+            "2025"=>15,
+            "2026"=>16,
+            "2027"=>16,
+            "2028"=>15,
+            "2029"=>15,
+            "2030"=>16,
+            "2031"=>16,
+            "2032"=>15,
+            "2033"=>15
+       
+    );
+/* Testing with static date*/
+$created='2017-07-17';
+//Placing year month and day in array
+$exploding=explode("-",$created);
+	if($exploding[1]>7) // if month is more than June
+	{ 
+		$newExpYear=$exploding[0]+1; // 2017+1
+	}
+	 elseif($exploding[1]>=7 && $exploding[2]<16)//if month>=7 AND days less than 16
+	 {
+		$newExpYear=$exploding[0]; //2017 =>same year
+    }
+    elseif($exploding[1]>=7 && $exploding[2]>16) //if month >=7 AND day greater than 16
+    {
+    	$newExpYear=$exploding[0]+1; //2018
+    }
+    elseif($exploding[1]>=7 && $exploding[2]>=16) //if month >=7 AND days >=16
+    {
+    	$newExpYear=$exploding[0]; //constant year
+    }
+    else
+    {
+    	$newExpYear=$exploding[0]; //else also same year
+    }
+    // var_dump($fiscalYear[2030]);
+    $dynamicDay=$fiscalYear[$newExpYear];
+echo $expiry= $newExpYear.'-'.'7'.'-'. $dynamicDay; 
+
+?>
